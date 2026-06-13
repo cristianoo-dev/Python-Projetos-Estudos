d8e09f9068171185f90ea10f3089637fbffcb681
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-06-13 15:47:57 -0300

    Cria primeira versão do projeto caixa eletrônico com operações básicas

--- projeto-01-caixa-eletronico/main.py
+++ projeto-01-caixa-eletronico/main.py
@@ -8,3 +8,24 @@ print('1 - Consultar saldo')
 print('2 - Depositar')
 print('3 - Sacar')
 print('4 - Sair')
+
+opção = int(input('Escolha sua opção: '))
+
+if opção == 1:
+    print(f'Seu saldo é igual a R${saldo:.2f} ')
+elif opção == 2:
+    deposito = float(input(('Qual valor deseja depositar? R$')))
+    saldo += deposito
+    print('Deposito Realizado com Sucesso!')
+    print(f'Saldo Atuazalido: R${saldo:.2f}')
+elif opção == 3:
+    saque = float(input('Qual valor deseja sacar? R$'))
+    if saque >  saldo:
+        print('Saldo Insuficiente')
+    else:
+        saldo -= saque
+        print(f'Saldo Atuzalido: R${saldo:.2f}')
+elif opção == 4:
+    print('Saindo...')
+else:
+    print('Opção Inválida')
