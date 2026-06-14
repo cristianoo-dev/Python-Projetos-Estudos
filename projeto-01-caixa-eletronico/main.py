021728fc614c497e17244553918c36ff12f7675a
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-06-14 15:52:42 -0300

    Implementa menu interativo com loop while

--- projeto-01-caixa-eletronico/main.py
+++ projeto-01-caixa-eletronico/main.py
@@ -3,29 +3,31 @@ print('CAIXA ELETRÔNICO')
 print('=' * 20)
 
 saldo = 1000
+opção = 0
+while opção != 4:
+    print('1 - Consultar saldo')
+    print('2 - Depositar')
+    print('3 - Sacar')
+    print('4 - Sair')
 
-print('1 - Consultar saldo')
-print('2 - Depositar')
-print('3 - Sacar')
-print('4 - Sair')
+    opção = int(input('Escolha sua opção: '))
 
-opção = int(input('Escolha sua opção: '))
-
-if opção == 1:
-    print(f'Seu saldo é igual a R${saldo:.2f} ')
-elif opção == 2:
-    deposito = float(input(('Qual valor deseja depositar? R$')))
-    saldo += deposito
-    print('Deposito Realizado com Sucesso!')
-    print(f'Saldo Atuazalido: R${saldo:.2f}')
-elif opção == 3:
-    saque = float(input('Qual valor deseja sacar? R$'))
-    if saque >  saldo:
-        print('Saldo Insuficiente')
+    if opção == 1:
+        print(f'Seu saldo é igual a R${saldo:.2f} ')
+    elif opção == 2:
+        deposito = float(input(('Qual valor deseja depositar? R$')))
+        saldo += deposito
+        print('Deposito Realizado com Sucesso!')
+        print(f'Saldo Atuazalido: R${saldo:.2f}')
+    elif opção == 3:
+        saque = float(input('Qual valor deseja sacar? R$'))
+        if saque >  saldo:
+            print('Saldo Insuficiente')
+        else:
+            saldo -= saque
+            print(f'Saldo Atuzalido: R${saldo:.2f}')
+    elif opção == 4:
+        print('Saindo...')
     else:
-        saldo -= saque
-        print(f'Saldo Atuzalido: R${saldo:.2f}')
-elif opção == 4:
-    print('Saindo...')
-else:
-    print('Opção Inválida')
+        print('Opção Inválida')
+    print('=-' *20)
