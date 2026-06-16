f37e56453702b8dc1163f6ff4f3ad9a06c5a40cf
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-06-15 21:40:08 -0300

    Refatora estrutura do menu e implementa validações de saída

--- projeto-01-caixa-eletronico/main.py
+++ projeto-01-caixa-eletronico/main.py
@@ -3,18 +3,20 @@ print('CAIXA ELETRÔNICO')
 print('=' * 20)
 
 saldo = 1000
-opção = 0
-while opção != 4:
-    print('1 - Consultar saldo')
-    print('2 - Depositar')
-    print('3 - Sacar')
-    print('4 - Sair')
 
-    opção = int(input('Escolha sua opção: '))
+print('1 - Consultar saldo')
+print('2 - Depositar')
+print('3 - Sacar')
+print('4 - Sair')
 
-    if opção == 1:
+continuar = True
+
+while continuar:
+    opção = str(input('Escolha sua opção: '))
+    
+    if opção == '1':
         print(f'Seu saldo é igual a R${saldo:.2f} ')
-    elif opção == 2:
+    elif opção == '2':
         deposito = float(input(('Qual valor deseja depositar? R$')))
         if deposito <= 0:
             print('O valor do depósito deve ser maior que zero.')
@@ -22,7 +24,7 @@ while opção != 4:
             saldo += deposito
             print('Depósito Realizado com Sucesso!')
             print(f'Saldo Atualizado: R${saldo:.2f}')
-    elif opção == 3:
+    elif opção == '3':
         saque = float(input('Qual valor deseja sacar? R$'))
         if saque <= 0:
             print('O valor do saque deve ser maior que zero')
@@ -30,10 +32,20 @@ while opção != 4:
             print('Saldo Insuficiente')
         else:
             saldo -= saque
-            print(f'Saldo Atuzalido: R${saldo:.2f}')
-
-    elif opção == 4:
-        print('Saindo...')
+            print(f'Saldo Atualizado: R${saldo:.2f}')
+    elif opção == '4':
+        sair = ' '
+        while sair not in 'SN':
+            resposta = str(input('Deseja Realmente Sair? [S/N] ')).strip().upper()
+            if resposta == '':
+                print('Digite S ou N.')
+            else:
+                sair = resposta[0]
+                if sair == 'S':
+                    continuar = False
+                    print('Saindo...')
     else:
         print('Opção Inválida')
     print('=-' *20)
+
+print('Obrigado e Volte Sempre!!!')
