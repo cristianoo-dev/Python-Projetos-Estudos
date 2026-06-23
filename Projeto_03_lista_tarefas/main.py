17578f42ce94a9d669686d761af5ef2bd54b5ea8
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-06-23 20:54:52 -0300

    Melhora lógica de validação e controle do menu na lista de tarefas

--- Projeto_03_lista_tarefas/main.py
+++ Projeto_03_lista_tarefas/main.py
@@ -11,15 +11,22 @@ print('''1 - Adionar Tarefa
 continuar = True
 
 while continuar:    
-    opção = input('Escolha uma opção: ')
-    if opção == '1':
-        tarefas.append(str(input('Digite a tarefa: ')))
+    opcao = input('Escolha uma opção: ')
+    if opcao == '1':
+        tarefa = input('Digite uma tarefa: ').strip()
+        if tarefa == '':
+            print('ERRO!!! A tarefa não pode estar vazia.')
+            continue        
+        tarefas.append(tarefa)
         
-    elif opção == '2':
-        for i, tarefa in enumerate(tarefas):
-            print(f'{i + 1} - {tarefa}')
+    elif opcao == '2':
+        if not tarefas:
+            print('Nenhuma tarefa foi adicionada.')
+        else:
+            for i, tarefa in enumerate(tarefas):
+                print(f'{i + 1} - {tarefa}')
 
-    elif opção == '3':
+    elif opcao == '3':
         sair = ' '
         while sair not in 'SN':
             resposta = str(input('Deseja Realmente Sair? [S/N] ')).strip().upper()
