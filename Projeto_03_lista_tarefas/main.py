078a75d727579cb7beb960b967de247f80744824
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-06-22 21:02:50 -0300

    Estrutura base do sistema de lista de tarefas com menu funcional

--- Projeto_03_lista_tarefas/main.py
+++ Projeto_03_lista_tarefas/main.py
@@ -8,4 +8,24 @@ print('''1 - Adionar Tarefa
 2 - Listar Tarefas
 3 - Sair''')
 
-opcao = input('Escolha uma opção: ')
+continuar = True
+
+while continuar:    
+    opção = str(input('Escolha uma opção: '))
+    if opção == '1':
+        tarefas.append(str(input('Digite a tarefa: ')))
+        
+    elif opção == '2':
+        print(tarefas)
+        
+    elif opção == '3':
+        sair = ' '
+        while sair not in 'SN':
+            resposta = str(input('Deseja Realmente Sair? [S/N] ')).strip().upper()
+            if resposta == '':
+                print('Digite S ou N.')
+            else:
+                sair = resposta[0]
+                if sair == 'S':
+                    continuar = False
+                    print('Saindo...')
