5139292718d3a9fa2e6d09dca08126e68ab9b230
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-06-24 22:00:42 -0300

    Adiciona comentários e melhora organização do sistema de lista de tarefas

--- Projeto_03_lista_tarefas/main.py
+++ Projeto_03_lista_tarefas/main.py
@@ -1,31 +1,37 @@
+# Lista que armazena as tarefas do sistema
 tarefas = []
 
 print('='*20)
 print('LISTA DE TAREFAS')
 print('='*20)
 
-print('''1 - Adionar Tarefa
-2 - Listar Tarefas
-3 - Sair''')
-
 continuar = True
 
-while continuar:    
+# Loop principal do sistema (mantém o menu ativo)
+while continuar:  
+    print('''1 - Adionar Tarefa
+2 - Listar Tarefas
+3 - Sair''')
+    # Adiciona nova tarefa após validação
     opcao = input('Escolha uma opção: ')
     if opcao == '1':
+        # Remove espaços e impede tarefas vazias
         tarefa = input('Digite uma tarefa: ').strip()
         if tarefa == '':
             print('ERRO!!! A tarefa não pode estar vazia.')
             continue        
         tarefas.append(tarefa)
-        
+        print('Tarefa adicionada.')
+
     elif opcao == '2':
         if not tarefas:
             print('Nenhuma tarefa foi adicionada.')
         else:
+            # Exibe tarefas com numeração
             for i, tarefa in enumerate(tarefas):
                 print(f'{i + 1} - {tarefa}')
 
+    # Confirma saída do sistema
     elif opcao == '3':
         sair = ' '
         while sair not in 'SN':
@@ -36,4 +42,6 @@ while continuar:
                 sair = resposta[0]
                 if sair == 'S':
                     continuar = False
-                    print('Saindo...')
+    print('-'*20) 
+print('Programa finalizado.')               
+ 
