128ac30d93f1b445b309fff1c80dc3a24de1cf33
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-06-25 21:10:04 -0300

    Cria estrutura inicial do Projeto 04 - Cadastro de Pessoas

---
+++ Projeto_04_cadastro_pessoas/main.py
@@ -0,0 +1,14 @@
+# Sistema de Cadastro de Pessoas
+
+print('=' * 25)
+print('CADASTRO DE PESSOAS')
+print('=' * 25)
+
+continuar = True
+
+while continuar:
+    print('''1 - Cadastrar Pessoa
+2 - Listar Pessoas
+3 - Sair''')
+
+    opcao = input('Escolha uma opção: ')
