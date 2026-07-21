9cdf71bce41ccdbcc84bc8214b683aa82f01487e
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-07-21 19:11:07 -0300

    Corrige caminho do arquivo JSON utilizando pathlib

--- Projeto06_BugTracker/json_utils.py
+++ Projeto06_BugTracker/json_utils.py
@@ -1,6 +1,10 @@
 import json
+from pathlib import Path
+
+ARQUIVO_BUGS = Path(__file__).parent / "bugs.json"
 
 def carregar_bugs():
-    with open("bugs.json") as arquivo:
+    with open(ARQUIVO_BUGS) as arquivo:
         bugs = json.load(arquivo)
+
     return bugs
