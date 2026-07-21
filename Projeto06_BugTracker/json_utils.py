e2af2bbece968a194b2bfefa77bd29b19319d575
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-07-21 19:33:10 -0300

    Implementa estrutura inicial de armazenamento de bugs em JSON

--- Projeto06_BugTracker/json_utils.py
+++ Projeto06_BugTracker/json_utils.py
@@ -6,5 +6,8 @@ ARQUIVO_BUGS = Path(__file__).parent / "bugs.json"
 def carregar_bugs():
     with open(ARQUIVO_BUGS) as arquivo:
         bugs = json.load(arquivo)
-
     return bugs
+
+def salvar_bugs(bugs):
+    with open(ARQUIVO_BUGS, "w") as arquivo:
+        json.dump(bugs, arquivo)
--- Projeto06_BugTracker/main.py
+++ Projeto06_BugTracker/main.py
@@ -1,5 +1,6 @@
 from json_utils import carregar_bugs
 
 bugs = carregar_bugs()
+bugs.append("Teste")
 
 print(bugs)
