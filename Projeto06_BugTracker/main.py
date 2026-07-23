1cb6cf8109a94578a75304c85f135051dc8a50ec
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-07-23 19:58:06 -0300

    Cria estrutura inicial do menu do BugTracker

--- Projeto06_BugTracker/main.py
+++ Projeto06_BugTracker/main.py
@@ -1,16 +1,52 @@
-from json_utils import carregar_bugs, salvar_bugs
+from json_utils import carregar_bugs
 
 bugs = carregar_bugs()
 
-novo_bug = {
-    "titulo": "Teste",
-    "descricao": "Teste de gravação",
-    "prioridade": "Alta",
-    "status": "Aberto"
-}
+continuar = True
 
-bugs.append(novo_bug)
+# Loop principal do sistema.
+while continuar:
+    print('='*15)
+    print('- Bug Traker -')
+    print('='*15)
+    print('1 - Cadastrar Bug')
+    print('2 - Listar Bugs')
+    print('3 - Buscar Bug')
+    print('4 - Alterar status')
+    print('5 - Remover Bug')
+    print('6 - Sair')
+    opcao = input('Escolha uma opção: ')
+    if opcao == '1':
+        print('Opção em desenvolvimento')
 
-salvar_bugs(bugs)
+    elif opcao == '2':
+        print('Opção em desenvolvimento')
 
-print(bugs)
+    elif opcao == '3':
+        print('Opção em desenvolvimento')
+
+    elif opcao == '4':
+        print('Opção em desenvolvimento')
+
+    elif opcao == '5':
+        print('Opção em desenvolvimento')
+
+    elif opcao == '6':
+        # Solicita confirmação antes de encerrar o programa.
+        while True:
+            resposta = input('Deseja realmente sair? [S/N]: '). strip().upper()
+            if resposta == '':
+                print('Digine S ou N.')
+            else:
+                resposta = resposta[0]
+                if resposta == 'S':
+                    print('Saindo...')
+                    continuar = False
+                    break
+                elif resposta == 'N':
+                    print('Operação Cancelada.')
+                    break
+    else:
+        print('Opção inválida.')
+    print('-'*20)
+print('Programa finalzado.')
