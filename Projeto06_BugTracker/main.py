c5d7a810f48a339157744edb12a1390899f87c0c
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-07-22 21:26:17 -0300

    test: validar leitura e gravação de dados em JSON

--- Projeto06_BugTracker/main.py
+++ Projeto06_BugTracker/main.py
@@ -1,6 +1,16 @@
-from json_utils import carregar_bugs
+from json_utils import carregar_bugs, salvar_bugs
 
 bugs = carregar_bugs()
-bugs.append("Teste")
+
+novo_bug = {
+    "titulo": "Teste",
+    "descricao": "Teste de gravação",
+    "prioridade": "Alta",
+    "status": "Aberto"
+}
+
+bugs.append(novo_bug)
+
+salvar_bugs(bugs)
 
 print(bugs)
