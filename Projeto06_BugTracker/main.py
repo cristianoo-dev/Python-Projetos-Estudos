b9557c7b0a2460d9263d3cc7abe15ba999ddfe58
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-07-24 21:14:06 -0300

    Implementa cadastro de bugs com salvamento em JSON

--- Projeto06_BugTracker/main.py
+++ Projeto06_BugTracker/main.py
@@ -1,4 +1,46 @@
-from json_utils import carregar_bugs
+from json_utils import carregar_bugs, salvar_bugs
+
+# Cadastra um novo bug solicitando as informações ao usuário.
+def cadastrar_bug():
+        bug = {}
+        # Solicita o nome do bug até que um valor válido seja informado.
+        while True:
+            titulo_bug = input('Título do bug: ').strip()
+            if titulo_bug == '':
+                print('ERRO! O título do bug não pode estar vazio.')
+            else:
+                bug['titulo'] = titulo_bug
+                break
+        # Solicita a descrição até que um valor válido seja informado.   
+        while True:
+            descricao = input('Descrição sobre o Bug: ').strip()
+            if descricao == '':
+                print('Erro! A descrioção não pode estar vazia.')  
+            else:
+                bug['descricao'] = descricao
+                break
+        # Exibe as opções de prioridade e valida a escolha do usuário.
+        while True:
+            print('\nEscolha a prioridade')
+            print('1 - Alta')
+            print('2 - Média')
+            print('3 - Baixa')
+            prioridade = input('Digite uma opção: ')
+            if prioridade == '1':
+                bug['prioridade'] = 'Alta'
+                break
+            elif prioridade == '2':
+                bug['prioridade'] = 'Média'
+                break
+            elif prioridade == '3':
+                bug['prioridade'] = 'Baixa'
+                break
+            else:
+                print('Opção inválida.')
+        bug['status'] = 'Aberto'
+        return bug
+
+
 
 bugs = carregar_bugs()
 
@@ -17,7 +59,10 @@ while continuar:
     print('6 - Sair')
     opcao = input('Escolha uma opção: ')
     if opcao == '1':
-        print('Opção em desenvolvimento')
+        bug = cadastrar_bug()
+        bugs.append(bug)
+        salvar_bugs(bugs)
+        print('\nBug cadastrado com sucesso.')
 
     elif opcao == '2':
         print('Opção em desenvolvimento')
